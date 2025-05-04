d337315d8ed8c89206db4b61eea2046bd4c5c916
kingkuys2123 <kylematthew.quitco@cit.edu>
2025-05-04 08:11:41 +0800

    Backend: Fixed JWT signature validation failure.

--- backend/src/main/java/com/servit/servit/util/JwtUtil.java
+++ backend/src/main/java/com/servit/servit/util/JwtUtil.java
@@ -3,10 +3,8 @@ package com.servit.servit.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -52,14 +50,12 @@ public class JwtUtil {
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
-        Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
-
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 3))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
 
