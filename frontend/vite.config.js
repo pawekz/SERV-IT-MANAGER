e992b684430c0aa8e6ea94ee01e0a5311d28a5c9
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2025-05-22 16:58:50 +0800

    Add preview configuration to vite.config.js for local development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from "vite";
+import {defineConfig, preview} from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173,
+    host: '0.0.0.0',
+    allowedHosts: ['servit-frontend-bfd2b4fjg8ayc2fn.southeastasia-01.azurewebsites.net']
+  },
+  define: {
+    global: 'window',
+  }
 });
