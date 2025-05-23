00273a772d3fb4f8ff6f82bc29ebf253a29c34c2
john48k ⌖ <johncanal300@gmail.com>
2025-05-23 12:27:41 +0800

    Signature Stepper is good to go now

--- frontend/src/pages/SignatureCapturePad/SignatureCapturePad.jsx
+++ frontend/src/pages/SignatureCapturePad/SignatureCapturePad.jsx
@@ -140,10 +140,10 @@ const SignatureCapturePad = () => {
                             </svg>
                         </span>
                     </li>
-                    <li className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700">
+                    <li className="flex w-full items-center text-[#33e407] dark:text-[#33e407] after:content-[''] after:w-full after:h-1 after:border-b after:border-[#33e407]/20 after:border-4 after:inline-block dark:after:border-[#33e407]/40">
                         <span
-                            className="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
-                            <svg className="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100" aria-hidden="true"
+                            className="flex items-center justify-center w-10 h-10 bg-[#33e407]/20 rounded-full lg:h-12 lg:w-12 dark:bg-[#33e407]/30 shrink-0">
+                            <svg className="w-4 h-4 text-[#33e407] lg:w-5 lg:h-5 dark:text-[#33e407]" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
                                 <path
                                     d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z"/>
@@ -271,3 +271,4 @@ const SignatureCapturePad = () => {
 }
 
 export default SignatureCapturePad
+
