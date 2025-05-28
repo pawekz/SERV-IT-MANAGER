70a4d0bec9419b336fbf9e9aad595e794e711c35
Paulo Carabuena <45958147+pawekz@users.noreply.github.com>
2025-05-28 20:57:14 +0800

    Add PartEnum and extend part attributes in DTO and Entity
    Introduce `PartEnum` to define part types, including `STANDARD` and `SUPPLIER_REPLACEMENT`. Update `PartResponseDTO` and `PartEntity` to include new fields: `quotationPart`, `sourceType`, and `isReserved`, enhancing part data structure and functionality.

---
+++ backend/src/main/java/com/servit/servit/enumeration/PartEnum.java
@@ -0,0 +1,10 @@
+package com.servit.servit.enumeration;
+
+public enum PartEnum {
+    /*
+    * STANDARD: Standard part, typically used in most repairs.
+    * SUPPLIER_REPLACEMENT: Part that is replaced by the supplier, often used when a specific part is not available or needs to be sourced from a different supplier.
+    */
+    STANDARD,
+    SUPPLIER_REPLACEMENT
+}
