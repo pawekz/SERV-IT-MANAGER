ac93d98a5c7298831feb106896df47108fec95b0
john48k ⌖ <johncanal300@gmail.com>
2025-06-16 13:46:42 +0800

    test

--- frontend/src/pages/RepairQueue/RepairQueue.jsx
+++ frontend/src/pages/RepairQueue/RepairQueue.jsx
@@ -350,7 +350,6 @@ const RepairQueue = () => {
                                     </p>
 
                                     // Resolved Repairs
-
                                 ) : (
                                     <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
                                         {warrantyRequests
